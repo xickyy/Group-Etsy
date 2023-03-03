9eb6a41da65c23623ed5202b2bbde5c240b45209
heidisong0814 <heidisong0814@gmail.com>
2023-03-02 22:17:37 -0800

    display search bar

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -1,27 +1,28 @@
+
 import React, {useState, useEffect} from 'react'
 import { allProductsThunk } from '../../store/products';
 import { useSelector, useDispatch } from "react-redux";
 
 
+
 const SearchBar = () => {
 
   const dispatch = useDispatch();
+  const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
-    const allProducts = await fetch('/api/products');
-    console.log('ALLLLLLLLLLLLLLLLLLLL',allProducts)
-    const data = await allProducts.json();
-    console.log('DATAAAAAAAAAAAAAAAAAAAA', data)
-    return data;
-  }
-
-  const products = getProducts()
-
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch('/api/products');
+      const data = await response.json();
+      setProducts(data.products);
+    }
+    getProducts()}, []);
 
 
   const [searchInput, setSearchInput] = useState("");
   // const products = ['pillow', 'necklace', 'chair']
   console.log('@@@@@@@@@@@', products)
+  
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -29,39 +30,37 @@ const SearchBar = () => {
   };
 
   if (searchInput.length > 0) {
-      products.filter((product) => {
-      return product.match(searchInput);
-  });
-  }
+    products.filter((product) => {
+    return product.title.match(searchInput);
+});
+}
 
-  return <div>
+const filteredProducts = products.filter(product => product.title.toLowerCase().includes(searchInput.toLowerCase()));
 
-<input
-   type="search"
-   placeholder="Search here"
-   onChange={handleChange}
-   value={searchInput} />
-
-{/* <table>
-  <tr>
-    <th>Country</th>
-    <th>Continent</th>
-  </tr>
-
-{products.map((product, *index*) => {
-
-<div>
-  <tr>
-    <td>{products}</td>
-    <td>{products}</td>
-  </tr>
-</div>
-
-})}
-</table> */}
-
-</div>
-
+return (
+  <div>
+    <input
+      type="search"
+      placeholder="Search here"
+      onChange={handleChange}
+      value={searchInput}
+    />
+    <table>
+      <thead>
+        <tr>
+          <th>Product</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredProducts.map((product, index) => (
+          <tr key={index}>
+            <td>{product.title}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 
 
 }
