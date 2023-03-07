cd4e667ff2fd024d44a0f54d83825fa1709dec62
alexandratai <alextai@alexs-air.lan>
2023-03-06 17:20:27 -0800

    Slight edit

--- react-app/src/components/EditReviewForm/index.js
+++ react-app/src/components/EditReviewForm/index.js
@@ -1,10 +1,9 @@
-import "./EditReviewForm.css";
+
 
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { editReviewThunk } from "../../store/reviews";
 import { useModal } from "../../context/Modal";
-// import { useParams } from "react-router-dom";
 
 const EditReviewForm = ({ review, productId }) => {
     const dispatch = useDispatch();
