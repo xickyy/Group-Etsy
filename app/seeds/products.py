f1022e538b4315885c0e1702d7279b542393f041
ricky <ricky_moore@ymail.com>
2023-03-07 21:15:12 -0800

    updated seeders

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -8,12 +8,26 @@ def seed_products():
         title='Gold Necklace', description='Perfect for pairing with any outfit, our Solid Gold Name Necklace offers a look that’s both chic and stylish.', price=18.90, image_URL="https://i.etsystatic.com/24042361/r/il/62c7e3/4221094131/il_1588xN.4221094131_ji8c.jpg", user_id=2)
     product3 = Product(
         title='Watercolor Print', description='Printable art is an easy and affordable way to personalize your home or office.', price=5.43, image_URL="https://i.etsystatic.com/14789656/r/il/a86632/1296942973/il_1588xN.1296942973_nlyi.jpg", user_id=3)
-    
+    product4 = Product(
+        title='Yakes Hat', description='A cool hat for cool people!', price=24.99, image_URL="https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/fdd635b9cd494d12a9e4ac8e015ebe19_9366/superlite-hat.jpg", user_id=1)
+    product5 = Product(
+        title='Glass Cups', description="These very fancy cups will break if you drop them, so don't... or do?", price=17.98, image_URL="https://m.media-amazon.com/images/I/71yKdvUjuIL.jpg", user_id=2)
+    product6 = Product(
+        title='Indigo Blue Japandi Art Print', description='Indigo Blue Japandi Wall Art Printable, Navy Abstract Art Print, Modern Geometric Poster, Bohemian Abstract Print, Stripes Curves Painting', price=6.50, image_URL="https://i.etsystatic.com/25191255/r/il/c7143b/3872381504/il_794xN.3872381504_jesw.jpg", user_id=3)
+    product7 = Product(
+        title='Gabes Cat', description='This cat is a package deal, comes with a hooman named Gabriel', price=999.99, image_URL="https://www.thesprucepets.com/thmb/vgvK9_iqmLpIHezaqZ_Mi5evmQM=/1600x0/filters:no_upscale():strip_icc()/black-and-white-cat-breeds-4843190-hero-8ea9ae92c05c46fb9495f789ca595f37.jpg", user_id=1)
+    product8 = Product(
+        title='Fancy Bowl', description='Large Bowl in Rustic White Concrete with Gold Leaf Interior', price=150.00, image_URL="https://i.etsystatic.com/5256837/r/il/eea7a5/4041213035/il_794xN.4041213035_epwa.jpg", user_id=1)
+    product9 = Product(
+        title='The Mask', description='*WEAR WITH CAUTION* side effects of wearing this mask may increase rizz ', price=42.50, image_URL="https://i.etsystatic.com/23074566/r/il/3f7320/2762529568/il_fullxfull.2762529568_1z5m.jpg", user_id=2)
+    product10 = Product(
+        title='Chakra Set', description='7 Chakra Stones + 1 Healing Crystals Necklace - Healing Stones Kit with Life Tree Necklace', price=10.79, image_URL="https://i.etsystatic.com/38981368/r/il/3c5670/4557800782/il_794xN.4557800782_hwb9.jpg", user_id=3)
 
-    products = [product1, product2, product3]
+
+    products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10]
     for product in products:
         db.session.add(product)
-    
+
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
@@ -27,5 +41,5 @@ def undo_products():
         db.session.execute(f"TRUNCATE table {SCHEMA}.products RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM products")
-        
+
     db.session.commit()
--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -8,8 +8,22 @@ def seed_reviews():
         body="Broke the first try!", stars=1, product_id=2, user_id=1)
     review3 = Review(
         body="Great quality, but got the wrong size.", stars=3, product_id=3, user_id=2)
+    review4 = Review(
+        body="After wearing this hat i noticed my coding skills increase!", stars=5, product_id=4, user_id=2)
+    review5 = Review(
+        body="just got these last week and i love them! Although I may or may not have broke them all already...", stars=4, product_id=5, user_id=3)
+    review6 = Review(
+        body="This really brings my whole livingroom together! although the quality could be better.", stars=4, product_id=6, user_id=1)
+    review7 = Review(
+        body="The cat is awesome! And Gabe is a huge help when it comes to debugging, 10/10 would recommend", stars=5, product_id=7, user_id=3)
+    review8 = Review(
+        body="I bought this for my boyfriend and he loved it till he found out how much i spent!", stars=3, product_id=8, user_id=2)
+    review9 = Review(
+        body="ITS PARTY TIME!! P-A-R-T-WHY? BECAUSE I GOTTA!!!", stars=5, product_id=9, user_id=1)
+    review10 = Review(
+        body="all my stones came broken!!", stars=1, product_id=10, user_id=2)
 
-    reviews = [review1, review2, review3]
+    reviews = [review1, review2, review3, review4, review5, review6, review7, review8, review9, review10]
     for review in reviews:
         db.session.add(review)
 
@@ -26,5 +40,5 @@ def undo_reviews():
         db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM reviews")
-        
+
     db.session.commit()
