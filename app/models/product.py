ab8a579a6e31e33667b7c8bcd1a6d8b86191df86
ricky <ricky_moore@ymail.com>
2023-03-10 15:44:20 -0800

    changed table names back to without the underscore

--- app/models/product.py
+++ app/models/product.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Product(db.Model):
-    __table_name__ = 'products'
+    __tablename__ = 'products'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
