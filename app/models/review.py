34446a72e42cf6b8dc27c0061dd33e6be5f19e94
ricky <ricky_moore@ymail.com>
2023-03-10 22:16:07 -0800

    fixed reviews table

--- app/models/review.py
+++ app/models/review.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Review(db.Model):
-    __tablename__: 'reviews'
+    __tablename__= 'reviews'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
