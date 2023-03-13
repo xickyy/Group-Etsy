a0f0d06f02333de3db1ee9663918764466b84d71
alexandratai <alextai@Alexs-Air.lan>
2023-03-12 19:14:08 -0700

    Added star icons to create review - Alex

--- react-app/src/components/CreateReviewForm/index.js
+++ react-app/src/components/CreateReviewForm/index.js
@@ -10,11 +10,61 @@ const CreateReviewForm = (productId) => {
 
   const [body, setBody] = useState("");
   const [stars, setStars] = useState(0);
+  const [firstStar, setFirstStar] = useState("fa-sharp fa-regular fa-star");
+  const [secondStar, setSecondStar] = useState("fa-sharp fa-regular fa-star");
+  const [thirdStar, setThirdStar] = useState("fa-sharp fa-regular fa-star");
+  const [fourthStar, setFourthStar] = useState("fa-sharp fa-regular fa-star");
+  const [fifthStar, setFifthStar] = useState("fa-sharp fa-regular fa-star");
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
   const updateBody = (e) => setBody(e.target.value);
-  const updateStars = (e) => setStars(e.target.value);
+  // const updateStars = (e) => setStars(e.target.value);
+
+  const handleFirstStar = () => {
+    setFirstStar("fa-sharp fa-solid fa-star");
+    setSecondStar("fa-sharp fa-regular fa-star");
+    setThirdStar("fa-sharp fa-regular fa-star");
+    setFourthStar("fa-sharp fa-regular fa-star");
+    setFifthStar("fa-sharp fa-regular fa-star");
+    setStars(1);
+  };
+
+  const handleSecondStar = () => {
+    setFirstStar("fa-sharp fa-solid fa-star");
+    setSecondStar("fa-sharp fa-solid fa-star");
+    setThirdStar("fa-sharp fa-regular fa-star");
+    setFourthStar("fa-sharp fa-regular fa-star");
+    setFifthStar("fa-sharp fa-regular fa-star");
+    setStars(2);
+  };
+
+  const handleThirdStar = () => {
+    setFirstStar("fa-sharp fa-solid fa-star");
+    setSecondStar("fa-sharp fa-solid fa-star");
+    setThirdStar("fa-sharp fa-solid fa-star");
+    setFourthStar("fa-sharp fa-regular fa-star");
+    setFifthStar("fa-sharp fa-regular fa-star");
+    setStars(3);
+  };
+
+  const handleFourthStar = () => {
+    setFirstStar("fa-sharp fa-solid fa-star");
+    setSecondStar("fa-sharp fa-solid fa-star");
+    setThirdStar("fa-sharp fa-solid fa-star");
+    setFourthStar("fa-sharp fa-solid fa-star");
+    setFifthStar("fa-sharp fa-regular fa-star");
+    setStars(4);
+  };
+
+  const handleFifthStar = () => {
+    setFirstStar("fa-sharp fa-solid fa-star");
+    setSecondStar("fa-sharp fa-solid fa-star");
+    setThirdStar("fa-sharp fa-solid fa-star");
+    setFourthStar("fa-sharp fa-solid fa-star");
+    setFifthStar("fa-sharp fa-solid fa-star");
+    setStars(5);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -50,14 +100,23 @@ const CreateReviewForm = (productId) => {
         />
 
         <p>How would you rate your experience?</p>
-        <input
+        {/* <input
           type="number"
           placeholder="Stars here"
           value={stars}
           onChange={updateStars}
           min="1"
           max="5"
-        />
+        /> */}
+
+        <div className="create-review-stars">
+        <i onClick={handleFirstStar} className={firstStar}></i>
+        <i onClick={handleSecondStar} className={secondStar}></i>
+        <i onClick={handleThirdStar} className={thirdStar}></i>
+        <i onClick={handleFourthStar} className={fourthStar}></i>
+        <i onClick={handleFifthStar} className={fifthStar}></i>
+        </div>
+
         <div>
           <button className="create-review-button" type="submit">
             Create New Review
